2bd3f2c5f28e72b5f2519d5dc4f0f064967297bb
goldbricklemon <9368670+goldbricklemon@users.noreply.github.com>
2026-01-08 07:37:44 +0100

    Basic point-in-polygon check ready. Rect-edge check still missing

--- src/09.py
+++ src/09.py
@@ -21,7 +21,7 @@ TEST_INPUT = \
 """
 
 TEST_RESULT_PART_ONE = 50
-TEST_RESULT_PART_TWO = 0
+TEST_RESULT_PART_TWO = 24
 
 def read_points(lines: list[str]) -> np.ndarray:
     return np.array([[int(s) for s in line.split(',')] for line in lines])
@@ -44,8 +44,74 @@ def solve_part_one(lines: list[str]) -> int:
     return np.max(rect_areas)
 
 
+def point_in_polygon(p: tuple[int, int], poly_points:np.ndarray) -> bool:
+    # Ray-casting algorithm using the even-odd rule
+    # Cast a horizontal ray from p to x-infinity and check number of poly edge intersections
+    # Uses image coordinate system (y axis down)!
+    px, py = p
+    n_points = poly_points.shape[0]
+    in_poly = False
+    for i in range(n_points):
+        ax, ay = poly_points[i-1]
+        bx, by = poly_points[i]
+
+        # Check if p is poly grid point
+        if p == (ax, ay) or p == (bx, by):
+            return True
+
+        # Check if poly edge is horizontal and contains p
+        if (ay == by == py) and (ax <= px <= bx or bx <= px <= ax):
+            return True
+        
+        # Check if ray can hit AB at all
+        if ay <= py <= by or by <= py <= ay:
+            # Special case: ray hits the lower of both points
+            # Ignore to avoid double-counting of this hit
+            if ( py == ay and by <= ay ) or ( py == by and ay <= by ):
+                continue
+
+            # Use cross-product to determine the side of AB on which p lies
+            cross = (ax - px) * (by - py) - (ay - py) * (bx - px)
+            if cross == 0:
+                # Ray intersects and is parallel to AB
+                return True
+            # In image coordinates, when a above b, then p must be right of AB to intersect
+            # In this case, cross < 0
+            if (ay < by) == (cross < 0):
+                in_poly = not in_poly
+    return in_poly
+
+
+
 def solve_part_two(lines: list[str]) -> int:
-    return 0
+    #poly = np.array([[1,2], [6,2], [6,4], [4,4], [4,6], [3,6], [3,5], [1,5]])
+    #point_in_polygon((0,3), poly)
+    #48:15
+
+    # points in (N, 2)
+    points = read_points(lines)
+    n_points = points.shape[0]
+    # point pairs in (N, N, 2)
+    pairs = np.tile(points[:, np.newaxis, :], reps=(1, n_points, 1))
+    # coord-wise differences within pairs
+    # (N, N, 2)
+    pair_diff = pairs - np.transpose(pairs, axes=(1, 0, 2))
+    # absolute differences (in x and y). +1 due to the way area is calulated in this puzzle
+    pair_diff = np.abs(pair_diff) + 1
+    # rect areas for each pair: (|dx| + 1) * (|dy| + 1)
+    rect_areas = pair_diff[:, :, 0] * pair_diff[:, :, 1]
+
+    max_area = 0
+    for i in range(n_points):
+        for j in range(i + 1, n_points):
+            if rect_areas[i,j] > max_area:
+                pi, pj = points[[i, j]]
+                edge2 = (pi[0], pj[1])
+                edge3 = (pj[0], pi[1])
+                if point_in_polygon(edge2, points) and point_in_polygon(edge3, points):
+                    max_area = rect_areas[i,j]
+
+    return max_area # Too high!
 
 
 if __name__ == '__main__':
